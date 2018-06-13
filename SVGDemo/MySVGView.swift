3d9143673cd3c5877e4c0b24bc8a05ea0f7121f3
WEI QIN <weiqin@WEIs-Mac-mini.local>
2018-06-13 16:59:26 +0800

    fix: layout error

--- SVGDemo/MySVGView.swift
+++ SVGDemo/MySVGView.swift
@@ -23,24 +23,28 @@ public class MySVGView: MacawView {
         }
     }
     
+    var svgNode: Node!
     var pinchGesture: UIPinchGestureRecognizer!
     var panGesture: UIPanGestureRecognizer!
-    var originalTrans: Transform!
-    var trans: Transform!
+    var originalTrans: Transform!           // svg原始大小的transform
+    var scaleAspectFitTrans: Transform!     // scaleAspectFit的transform
+    var trans: Transform!                   // 当前的transform
     
     public init(template: String, frame: CGRect) {
         super.init(frame: frame)
         self.template = template
         if let node = try? SVGParser.parse(path: template) {
+            // gesture
             pinchGesture = UIPinchGestureRecognizer.init(target: self, action: #selector(handlePinch(gesture:)))
             panGesture = UIPanGestureRecognizer.init(target: self, action: #selector(handlePan(gesture:)))
             panGesture.maximumNumberOfTouches = 2
             self.addGestureRecognizer(pinchGesture)
             self.addGestureRecognizer(panGesture)
             addTap(node: node)
-            self.node = node
+            // layout
+            svgNode = node
         }
-        originalTrans = node.place
+        originalTrans = Transform.init(m11: node.place.m11, m12: node.place.m12, m21: node.place.m21, m22: node.place.m22, dx: node.place.dx, dy: node.place.dy)
     }
     
     public init(node: Node = Group(), frame: CGRect) {
@@ -58,28 +62,8 @@ public class MySVGView: MacawView {
 
 }
 
-// MARK: -
+// MARK: Business
 extension MySVGView {
-    public func addTap(node: Node) {
-        if let group = node as? Group {
-            for child in group.contents {
-                addTap(node: child)
-            }
-        }else if let shape = node as? Shape {
-            shape.onTap { [weak self] (tapEvent) in
-                guard let strongSelf = self else { return }
-//                strongSelf.replaceColors(node: shape)
-                strongSelf.node.place = strongSelf.node.place.move(dx: 100, dy: 0)
-                print("tap(node: \(node.tag))")
-            }
-            shape.onLongTap { [weak self] (longPressEvent) in
-                guard let strongSelf = self else { return }
-                strongSelf.eyedropper(node: shape)
-                print("long press(node: \(node.tag))")
-            }
-        }
-    }
-    
     public func replaceColors(node: Node) -> Bool {
 //        if let group = node as? Group {
 //            for child in group.contents {
@@ -183,6 +167,7 @@ extension MySVGView {
 
 }
 
+// MARK: Gesture
 extension MySVGView {
     @objc func handlePinch(gesture: UIPinchGestureRecognizer) {
 //        print("pinch")
@@ -191,30 +176,14 @@ extension MySVGView {
             let touch0 = gesture.location(ofTouch: 0, in: self)
             let touch1 = gesture.location(ofTouch: 1, in: self)
             let anchor = CGPoint.init(x: (touch0.x + touch1.x) / 2 , y: (touch0.y + touch1.y) / 2)
-            // 限制缩放比例: (1.0 ~ 8.0)
-            var pinchScale = Double(gesture.scale)
-            var scale: Double = 1.0
-            if let t = trans, let ot = originalTrans {
-                scale = t.m11 / ot.m11
-            }
-            if pinchScale >= 1.0 {  // 放大
-                if scale >= 8.0 {
-                    pinchScale = 1.0
-                }
-            }else {                 // 缩小
-                if scale <= 1.0 {
-                    node.place = originalTrans
-                    trans = originalTrans
-                    return
-                }
-            }
+            let pinchScale = Double(gesture.scale)
             if let t = trans {
                 node.place = t.move(dx: Double(anchor.x) * (1.0 - pinchScale), dy: Double(anchor.y) * (1.0 - pinchScale)).scale(sx: pinchScale, sy: pinchScale)
             }else {
                 node.place = Transform.move(dx: Double(anchor.x) * (1.0 - pinchScale), dy: Double(anchor.y) * (1.0 - pinchScale)).scale(sx: pinchScale, sy: pinchScale)
             }
         }else if gesture.state == UIGestureRecognizerState.ended {
-            trans = node.place
+            trans = Transform.init(m11: node.place.m11, m12: node.place.m12, m21: node.place.m21, m22: node.place.m22, dx: node.place.dx, dy: node.place.dy)
         }
     }
     
@@ -229,18 +198,187 @@ extension MySVGView {
                 return
             }
             let translation = gesture.translation(in: gesture.view)
-            var scale: Double = 1.0
+            var scaleToOriginalSVG: Double = Double(bounds.size.width / 850)
             if let t = trans, let ot = originalTrans {
-                scale = t.m11 / ot.m11
+                scaleToOriginalSVG = t.m11 / ot.m11
             }
-            node.place = node.place.move(dx: Double(translation.x / CGFloat(scale)), dy: Double(translation.y / CGFloat(scale)))
+            print(scaleToOriginalSVG)
+            node.place = node.place.move(dx: Double(translation.x / CGFloat(scaleToOriginalSVG)), dy: Double(translation.y / CGFloat(scaleToOriginalSVG)))
             gesture.setTranslation(CGPoint.zero, in: gesture.view)
+            trans = Transform.init(m11: node.place.m11, m12: node.place.m12, m21: node.place.m21, m22: node.place.m22, dx: node.place.dx, dy: node.place.dy)
         }else if gesture.state == UIGestureRecognizerState.ended {
             if gesture.numberOfTouches == 1 && penMode == 1 {
                 return
             }
-            trans = node.place
         }
+    }
+    
+    public func addTap(node: Node) {
+        if let group = node as? Group {
+            for child in group.contents {
+                addTap(node: child)
+            }
+        }else if let shape = node as? Shape {
+            shape.onTap { [weak self] (tapEvent) in
+                guard let strongSelf = self else { return }
+                strongSelf.replaceColors(node: shape)
+                print("tap(node: \(node.tag))")
+            }
+            shape.onLongTap { [weak self] (longPressEvent) in
+                guard let strongSelf = self else { return }
+                strongSelf.eyedropper(node: shape)
+                print("long press(node: \(node.tag))")
+            }
+        }
+    }
+}
+
+// MARK: Layout
+extension MySVGView {
+    open override var contentMode: MViewContentMode {
+        didSet {
+            render()
+        }
+    }
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        render()
+    }
+    
+    fileprivate func render() {
+        guard let svgNode = self.svgNode else {
+            return
+        }
+        let viewBounds = self.bounds
+        /*
+         此处svgNode的bounds写成了固定大小850x850。
+         */
+        let nodeBounds = Rect.init(x: 0, y: 0, w: 850, h: 850).cgRect() // svg原始尺寸
+        let svgWidth = nodeBounds.origin.x + nodeBounds.width
+        let svgHeight = nodeBounds.origin.y + nodeBounds.height
+        
+        let viewAspectRatio = viewBounds.width / viewBounds.height
+        let svgAspectRatio = svgWidth / svgHeight
+        
+        let scaleX = viewBounds.width / svgWidth
+        let scaleY = viewBounds.height / svgHeight
+        
+        switch self.contentMode {
+        case .scaleToFill:
+            svgNode.place = Transform.scale(
+                sx: Double(scaleX),
+                sy: Double(scaleY)
+            )
+        case .scaleAspectFill:
+            let scaleX, scaleY: CGFloat
+            if viewAspectRatio > svgAspectRatio {
+                scaleX = viewBounds.width / svgWidth
+                scaleY = viewBounds.width / (svgWidth / svgAspectRatio)
+            } else {
+                scaleX = viewBounds.height / (svgHeight / svgAspectRatio)
+                scaleY = viewBounds.height / svgHeight
+            }
+            let calculatedWidth = svgWidth * scaleX
+            let calculatedHeight = svgHeight * scaleY
+            svgNode.place = Transform.move(
+                dx: Double(viewBounds.width / 2 - calculatedWidth / 2),
+                dy: Double(viewBounds.height / 2 - calculatedHeight / 2)
+                ).scale(
+                    sx: Double(scaleX),
+                    sy: Double(scaleX)
+            )
+        case .scaleAspectFit:
+            let calculatedXWidth = scaleX * svgWidth
+            let calculatedXHeight = scaleX * svgHeight
+            let calculatedYWidth = scaleY * svgWidth
+            let calculatedYHeight = scaleY * svgHeight
+            
+            if calculatedXWidth <= viewBounds.width && calculatedXHeight <= viewBounds.height {
+                svgNode.place = Transform.move(
+                    dx: Double(viewBounds.midX - calculatedXWidth / 2),
+                    dy: Double(viewBounds.midY - calculatedXHeight / 2)
+                    ).scale(
+                        sx: Double(scaleX),
+                        sy: Double(scaleX)
+                )
+            } else if calculatedYWidth <= viewBounds.width && calculatedYHeight <= viewBounds.height {
+                svgNode.place = Transform.move(
+                    dx: Double(viewBounds.midX - calculatedYWidth / 2),
+                    dy: Double(viewBounds.midY - calculatedYHeight / 2)
+                    ).scale(
+                        sx: Double(scaleY),
+                        sy: Double(scaleY)
+                )
+            }
+            scaleAspectFitTrans = Transform.init(m11: svgNode.place.m11, m12: svgNode.place.m12, m21: svgNode.place.m21, m22: svgNode.place.m22, dx: svgNode.place.dx, dy: svgNode.place.dy)
+        case .center:
+            svgNode.place = Transform.move(
+                dx: Double(getMidX(viewBounds, nodeBounds)),
+                dy: Double(getMidY(viewBounds, nodeBounds))
+            )
+        case .top:
+            svgNode.place = Transform.move(
+                dx: Double(getMidX(viewBounds, nodeBounds)),
+                dy: 0
+            )
+        case .bottom:
+            svgNode.place = Transform.move(
+                dx: Double(getMidX(viewBounds, nodeBounds)),
+                dy: Double(getBottom(viewBounds, nodeBounds))
+            )
+        case .left:
+            svgNode.place = Transform.move(
+                dx: 0,
+                dy: Double(getMidY(viewBounds, nodeBounds))
+            )
+        case .right:
+            svgNode.place = Transform.move(
+                dx: Double(getRight(viewBounds, nodeBounds)),
+                dy: Double(getMidY(viewBounds, nodeBounds))
+            )
+        case .topLeft:
+            break
+        case .topRight:
+            svgNode.place = Transform.move(
+                dx: Double(getRight(viewBounds, nodeBounds)),
+                dy: 0
+            )
+        case .bottomLeft:
+            svgNode.place = Transform.move(
+                dx: 0,
+                dy: Double(getBottom(viewBounds, nodeBounds))
+            )
+        case .bottomRight:
+            svgNode.place = Transform.move(
+                dx: Double(getRight(viewBounds, nodeBounds)),
+                dy: Double(getBottom(viewBounds, nodeBounds))
+            )
+        case .redraw:
+            break
+        }
+
+        self.node = svgNode
+    }
+    
+    fileprivate func getMidX(_ viewBounds: CGRect, _ nodeBounds: CGRect) -> CGFloat {
+        let viewMidX = viewBounds.midX
+        let nodeMidX = nodeBounds.midX + nodeBounds.origin.x
+        return viewMidX - nodeMidX
+    }
+    
+    fileprivate func getMidY(_ viewBounds: CGRect, _ nodeBounds: CGRect) -> CGFloat {
+        let viewMidY = viewBounds.midY
+        let nodeMidY = nodeBounds.midY + nodeBounds.origin.y
+        return viewMidY - nodeMidY
+    }
+    
+    fileprivate func getBottom(_ viewBounds: CGRect, _ nodeBounds: CGRect) -> CGFloat {
+        return viewBounds.maxY - nodeBounds.maxY + nodeBounds.origin.y
+    }
+    
+    fileprivate func getRight(_ viewBounds: CGRect, _ nodeBounds: CGRect) -> CGFloat {
+        return viewBounds.maxX - nodeBounds.maxX + nodeBounds.origin.x
     }
 }
 
--- SVGDemo/ViewController.swift
+++ SVGDemo/ViewController.swift
@@ -19,29 +19,10 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor.yellow
         
         let svgFrame = CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.width)
-        
         mySVGView = MySVGView.init(template: "zen", frame: svgFrame)
         mySVGView.contentMode = .scaleAspectFit
-
         view.addSubview(mySVGView)
     }
     
 
 }
-
-//extension ViewController: UIScrollViewDelegate {
-//    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-//        return mySVGView
-//    }
-//
-//    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
-//        mySVGView.layoutSubviews()
-//        //        if let recognizer = scrollView.pinchGestureRecognizer {
-//        //            let location = recognizer.location(in: scrollView)
-//        //            let scale = Double(recognizer.scale)
-//        //            let anchor = Point(x: Double(location.x), y: Double(location.y))
-//        //            mySVGView.node.place = Transform.move(dx: anchor.x * (1.0 - scale), dy: anchor.y * (1.0 - scale)).scale(sx: scale, sy: scale)
-//        //        }
-//    }
-//}
-
